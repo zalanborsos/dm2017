f5e00e11fc97cafedf41b0686af11b9e92b7709d
Zalan Borsos <zalan.borsos@inf.ethz.ch>
2017-10-19 12:10:00 +0200

    Gradient descent tutotial init

---
+++ gradient_descent_tutorial/plots.py
@@ -0,0 +1,53 @@
+import matplotlib
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d, Axes3D 
+
+def initial_plot(f):
+    matplotlib.rcParams['xtick.direction'] = 'out'
+    matplotlib.rcParams['ytick.direction'] = 'out'
+
+    delta = 0.025
+    x = np.arange(-5.0, 5.0, delta)
+    y = np.arange(-5.0, 5.0, delta)
+    X, Y = np.meshgrid(x, y)
+    Z = f([X, Y])
+
+    fig = plt.figure(figsize=(20, 6))
+
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+
+    ax.plot_surface(X, Y, Z, rstride=4, cstride=4, alpha=0.25)
+    cset = ax.contour(X, Y, Z, zdir='z', offset=0, cmap=cm.coolwarm)
+
+    ax.set_xlim3d(-5, 5)
+    ax.set_ylim3d(-5, 5)
+    ax.set_zlim3d(0, 10)
+
+    ax2 = fig.add_subplot(1, 2, 2)
+
+    CS = ax2.contour(X, Y, Z)
+    ax2.clabel(CS, inline=1, fontsize=10, cmap=cm.coolwarm)
+    ax2.set_xlabel('$x_0$')
+    ax2.set_ylabel('$x_1$')
+    ax2.set_title('$f(\mathbf{x})$')
+
+def trajectory_visualization(f, n_iter, trajectory):
+    delta = 0.025
+    x = np.arange(-5.0, 5.0, delta)
+    y = np.arange(-5.0, 5.0, delta)
+    X, Y = np.meshgrid(x, y)
+    Z = f([X, Y])
+    
+    fig = plt.figure(figsize=(20, 5))
+    ax = fig.add_subplot(1, 2, 2)
+        
+    CS = ax.contour(X, Y, Z)
+    ax.clabel(CS, inline=1, fontsize=10, cmap=cm.coolwarm)
+    ax.set_xlabel('$x_0$')
+    ax.set_ylabel('$x_1$')
+    ax.set_title('{}'.format(n_iter))
+    ax.plot(trajectory[:n_iter, 0], trajectory[:n_iter, 1], '-o', markersize=10, color='red')
+   
+    
